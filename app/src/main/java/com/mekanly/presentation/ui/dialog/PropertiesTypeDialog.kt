e5715a86b6d83293a7005e836656d4ec23daad1b
Otabek <bekorazymbetow@gmail.com>
2024-12-17 21:17:11 +0500

    PropertiesTypeDialog

--- app/src/main/java/com/mekanly/presentation/ui/dialog/PropertiesTypeDialog.kt
+++ app/src/main/java/com/mekanly/presentation/ui/dialog/PropertiesTypeDialog.kt
@@ -6,18 +6,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mekanly.R
+import com.mekanly.databinding.FragmentDialogEmlaklerBinding
 
 class PropertiesTypeDialog : Fragment() {
+    private lateinit var binding: FragmentDialogEmlaklerBinding
+    private var selectedButton: View? = null
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDialogEmlaklerBinding.inflate(inflater, container, false)
+
+
+        val buttons = with(binding) {
+            listOf(
+                btnKwartira, btnKottej, btnElitka, btnPolElitka, btnDacha, btnPlanJay
+            )
+        }
+
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                // Если текущая кнопка еще не выбрана
+                if (selectedButton != button) {
+                    // Сбрасываем предыдущую выбранную кнопку
+                    selectedButton?.setBackgroundResource(R.drawable.emlakler_btn_bg)
+
+                    // Устанавливаем новую выбранную кнопку
+                    selectedButton = button
+                    button.setBackgroundResource(R.drawable.selected_emlakler_btn_bg)
+                }
+
+
+            }
+        }
+
+        return binding.root
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dialog_emlakler, container, false)
-    }
-
 }
+
