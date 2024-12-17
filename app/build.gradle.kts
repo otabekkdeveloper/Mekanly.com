e5418ce9dfa86e93edc756d7f3b54b8d7a97ad6d
Otabek <bekorazymbetow@gmail.com>
2024-12-17 21:17:30 +0500

    PropertiesTypeDialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,49 +51,33 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     //navigation
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-
     //roundedImageView
     implementation("com.makeramen:roundedimageview:2.3.0")
-
     //viewPager
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
-
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
-
     //retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.8.9")
-
-
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
     implementation ("com.squareup.picasso:picasso:2.8")
-
     implementation ("com.google.android.material:material:1.10.0")
-
     //glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-
-
     //splash Screen
     implementation ("androidx.core:core-splashscreen:1.0.0")
-
     implementation ("com.google.android.material:material:1.11.0")
-
     implementation ("com.google.android.material:material:1.9.0")
 
 
-
-
-
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.3")
 
 
 }
