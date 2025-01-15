6ae0b2cb64ef54e5014497117805fa9b2c1bf691
Otabek <bekorazymbetow@gmail.com>
2025-01-15 20:09:38 +0500

    new version in Properties Fragment

--- app/src/main/java/com/mekanly/presentation/ui/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/mekanly/presentation/ui/fragments/search/SearchFragment.kt
@@ -9,12 +9,11 @@ import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mekanly.R
-import com.mekanly.presentation.ui.adapters.pagerAdapters.SearchViewAdapter
 import com.mekanly.databinding.FragmentSearchBinding
 
 
 class SearchFragment : Fragment() {
-    private lateinit var viewPagerAdapter: SearchViewAdapter
+
     private lateinit var binding: FragmentSearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,40 +22,16 @@ class SearchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
 
 
-        // Настраиваем ViewPager2 с адаптером
-        viewPagerAdapter = SearchViewAdapter(requireActivity())
-        binding.viewPager.adapter = viewPagerAdapter
 
-        // Связываем TabLayout с ViewPager2 и добавляем иконки
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when (position) {
-                0 -> {
-                    tab.text = "EMLÄKLER"
-                }
-
-                1 -> {
-                    tab.text = "EMLÄK GÖZLEÝÄNLER"
-                }
-            }
-        }.attach()
-
-        binding.outlinedButton.setOnClickListener{
-            findNavController().navigate(R.id.action_homeFragment_to_filterFragment)
-        }
-
-
-        disableSwipe(binding.viewPager)
 
         return binding.root
     }
 
-    private fun disableSwipe(viewPager: ViewPager2) {
-        viewPager.isUserInputEnabled = false // Полное отключение свайпов
-    }
+
 
 
 
